789f5f9ea21f8d416d66d9bbd34f6977c50daf94
N Venkat <whenkey123@gmail.com>
2022-04-03 00:49:27 +0530

    Updated LCD Test Code

--- NodeMCU Codes/LCD_Test/LCD_Test.ino
+++ NodeMCU Codes/LCD_Test/LCD_Test.ino
@@ -2,16 +2,23 @@
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 void setup() {
-	lcd.begin();
-	lcd.backlight();
+  lcd.begin();
+  lcd.backlight();
   lcd.clear();
-	lcd.setCursor(0, 0);
-  lcd.print("ABCDEFGHIJKLMNOP");
-  lcd.setCursor(0, 1);
-  lcd.print("QRSTUVWXYZ123456");
 }
  
 void loop() {
+  for (int letter = 0; letter < (sizeof letters) -1; letter++) {
+    for (int row = 0; row < 2; row++) {
+      for (int column = 0; column < 16; column++) {
+        lcd.setCursor(column, row);
+        lcd.print(letters[letter]);
+      }
+    }
+    delay(500);
+    lcd.clear();
+  }
 }
