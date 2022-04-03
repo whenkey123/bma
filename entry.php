a6d02f0a054e8e1c0b2e66b2679e57f5ed44d6c7
N Venkat <whenkey123@gmail.com>
2022-04-03 19:23:59 +0530

    Add intime-late logic with 10:30 AM Max Intime

--- entry.php
+++ entry.php
@@ -10,7 +10,12 @@ if (isset($_GET['fingerprint_id'])) {
     $phone=$student_details['phone'];
     $department=$student_details['department'];
     $current_time=time();
-    $sql="INSERT INTO `student_attendance` (`id`, `roll_number`, `name`, `phone`, `department`, `entry_time`) VALUES (NULL, '$roll_number', '$name', '$phone', '$department', '$current_time');";
+    $status='intime';
+    $final_intime = DateTime::createFromFormat('m/d/Y H:i:s', date("m/d/Y").' 10:30:00')->getTimestamp();
+    if ($current_time > $final_intime) {
+      $status='late';
+    }
+    $sql="INSERT INTO `student_attendance` (`id`, `roll_number`, `name`, `phone`, `department`, `entry_time`, `status`) VALUES (NULL, '$roll_number', '$name', '$phone', '$department', '$current_time', '$status');";
     if (mysqli_query($con, $sql)) {
       echo("Attendance Taken");
     }else {
