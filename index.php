bcc545764d0085043231bd576f4bfcce881e6920
N Venkat <whenkey123@gmail.com>
2022-04-15 17:02:55 +0530

    add common css and hide 000webhost banner

--- index.php
+++ index.php
@@ -33,8 +33,7 @@ if (isset($_POST['entry_type']) and $_POST['entry_type']!="ALL") {
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
       <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-      <style media="screen">
-      </style>
+      <link rel="stylesheet" href="css/common.css">
    </head>
    <body>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
--- students.php
+++ students.php
@@ -41,6 +41,7 @@ if($_SERVER['REQUEST_METHOD']=="POST") {
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/common.css">
       <style media="screen">
         .align-right { text-align: right; }
       </style>
